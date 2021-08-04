4fbfe05e2cf634ef3bd8dd3b8e3156ad4b0fb8d4
CharlesPhillipsHouston <charles@spaceflightresearch.net>
2021-08-03 20:54:00 -0500

    Initial Commit

---
+++ images/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  images
+//
+//  Created by Charles Phillips on 8/3/21.
+//  Copyright © 2021 Charles Phillips. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
